d9a9ae7e629d6c81ed31cc8ea8179ddc3ffcc8ff
Marcin <marcin.lazar@pm.me>
2026-09-14 20:01:44 +0200

    fix dark mode

--- apps/playground/src/app/app.config.ts
+++ apps/playground/src/app/app.config.ts
@@ -13,6 +13,9 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
+        options: {
+            darkModeSelector: 'none'
+        }
       },
     }),
     provideMaplibreWorker('maplibre-gl-worker.mjs'),
