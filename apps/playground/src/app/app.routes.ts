016f32e221ff1f8a384316f6e16973df972a0ddb
Marcin <marcin.lazar@pm.me>
2026-09-14 15:14:53 +0200

    update docs page

--- apps/playground/src/app/app.routes.ts
+++ apps/playground/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Route } from '@angular/router';
-import { HereComponent } from '../pages/here/here.component';
-import { LeafletComponent } from '../pages/leaflet/leaflet.component';
 
 export const appRoutes: Route[] = [
-    { path: 'maplibre', component: HereComponent },
-    { path: 'leaflet', component: LeafletComponent },
+    { path: 'maplibre', loadComponent: () => import('../pages/here/here.component').then(({ HereComponent }) => HereComponent) },
+    { path: 'leaflet', loadComponent: () => import('../pages/leaflet/leaflet.component').then(({ LeafletComponent }) => LeafletComponent) },
     { path: '**', redirectTo: '/maplibre' }
 ];
